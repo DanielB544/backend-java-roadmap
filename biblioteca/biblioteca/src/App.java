eb0b72f4789c29b6a1feb8e59701b3a192b5b6c2
Daniel Rodriguez Serna <danielb544@hotmail.com>
2026-09-09 23:32:32 -0600

    feat: completar entidad Libro

--- biblioteca/biblioteca/src/App.java
+++ biblioteca/biblioteca/src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Libro libro = new Libro(1, "Hobbit", "J.R.R. Tolkien", 1937, true);
+        Libro libro2 = new Libro(2, "Clean Code", "Robert C. Martin", 2008, true);
+        Libro libro3 = new Libro(3, "1984", "George Orwell");
+        
+        libro.prestar();
+        libro.devolver();
+        /*System.out.println("El titulo es: " + libro.obtenerTitulo());
+        System.out.println(libro.libroDisponible());
+        System.out.println("Libro creado correctamente");*/
+
+        System.out.println( "ID: " + libro.getId() + "\n" +
+                            "Título: " + libro.getTitulo() + "\n" +
+                            "Autor: " + libro.getAutor() + "\n" +
+                            "Año: " + libro.getAnio() + "\n" +
+                            "Disponible: " + (libro.isDisponible() ? "Sí" : "No") + "\n");
     }
 }
